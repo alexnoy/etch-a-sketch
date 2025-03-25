3d81220cffcba616e087fc79dd4941cb5cdc2268
alexnoy <alexander.noy123@gmail.com>
2025-03-24 21:09:53 -0400

    Create eventListener for clear button
    Added a click eventListener so that when the clear button is clicked the added backgroundColor will be removed.

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ for (let i = 0; i < 256; i++) {
 }
 
 const grid = document.querySelectorAll('#grid')
-console.log(grid);
 grid.forEach(cell => cell.addEventListener('mouseover', function(){
     cell.style.backgroundColor = 'grey';
 }))
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', function() {
+    grid.forEach(cell => cell.style.backgroundColor = '');
+})
