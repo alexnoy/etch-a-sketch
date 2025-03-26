769529ed9d2f9f6b7b28957b8ffd93485a6715ac
alexnoy <alexander.noy123@gmail.com>
2025-03-25 23:00:37 -0400

    Create resize function
    Created a function that allows the user to change the size of the grid

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ clear.addEventListener('click', function() {
 });
 
 const resize = document.querySelector('#resize');
+
+resize.addEventListener('click', function() {
+    let size = prompt('Choose a grid size: 1-100');
+    if (size > 100 || size < 1) {
+        size = 16;
+        alert('I said 1-100');
+    }
+
+    grid.forEach(cell => cell.remove());
+
+    createGrid(size ** 2);
+
+    grid = document.querySelectorAll('#grid');
+    
+    grid.forEach(cell => cell.style.flexBasis = `${100 / size}%`)
+    grid.forEach(cell => cell.addEventListener('mouseover', function(){
+        cell.style.backgroundColor = 'grey';
+    }));
+});
