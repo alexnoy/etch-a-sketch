0908dae6e25fddc1e8af6c09683cf8a9a39a5b0d
alexnoy <alexander.noy123@gmail.com>
2025-03-25 22:54:30 -0400

    Add resize button to a variable
    Created a variable that is attached to the resize button

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(divs);
 }
 
-const grid = document.querySelectorAll('#grid')
+let grid = document.querySelectorAll('#grid');
 grid.forEach(cell => cell.addEventListener('mouseover', function(){
     cell.style.backgroundColor = 'grey';
-}))
+}));
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', function() {
     grid.forEach(cell => cell.style.backgroundColor = '');
-})
+});
+
+const resize = document.querySelector('#resize');
